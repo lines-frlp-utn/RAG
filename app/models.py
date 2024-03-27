fb250b1930be923c145bc29e3d7b8c65470f5f41
tomaszbk-pc <zubik.tomas@gmail.com>
2024-03-26 23:45:53 -0300

    feat: :sparkles: functioning llm model

--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
-from langchain.embeddings import SentenceTransformerEmbeddings
+# from langchain.embeddings import SentenceTransformerEmbeddings
 from langchain.llms import CTransformers
 
 """This model maps sentences & paragraphs to a 384 dimensional dense vector space
 and can be used for tasks like clustering or semantic search.
 """
+
 EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2"
 LLM_MISTRAL7B = "TheBloke/Mistral-7B-Instruct-v0.2-code-ft-GGUF"
-# C:\Users\usuario\.cache\huggingface\hub
-LLM_LLAMA_BLOKE = "TheBloke/Llama-2-13B-Ensemble-v5-GGUF"
 
-embedding_model = SentenceTransformerEmbeddings(model_name=EMB_SBERT_MINILM)
+# embedding_model = SentenceTransformerEmbeddings(model_name=EMB_SBERT_MINILM)
 
 """Creates the LLMS model that will generate contextualized embeddings"""
 config = {
@@ -19,12 +18,15 @@ config = {
     "temperature": 0.0,
     "top_k": 10,
 }
-# llm = CTransformers(
-#     model=LLM_MISTRAL7B,
-#     model_file="mistral-7b-instruct-v0.2-code-ft.Q5_K_M.gguf",
-#     model_type="llama",
-#     config=config,
-# )
+llm = CTransformers(
+    model=LLM_MISTRAL7B,
+    # model_file="mistral-7b-instruct-v0.2-code-ft.Q5_K_M.gguf",
+    # model_type="llama",
+    config=config,
+    device="cuda",
+)
+
+print(llm("where is argentina?"))
 
 prompt_template = """Conteste la siguiente pregunta basandose solamente en el contexto provisto:
 Si no sabes la respuesta, sólo di que no sabes, no trates de crearla. Siempre di "gracias por preguntar!".
