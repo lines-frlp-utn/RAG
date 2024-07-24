0493c084b9235d8683f25adc35c1613829fa270c
NachoL24 <ignaciolances24@gmail.com>
2024-07-24 17:59:18 -0300

    added fastapi

--- chainlit_app/app/config.py
+++ chainlit_app/app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     MODEL_URL: str = "http://localhost"
     MODEL_PORT: int = 8007
+    DB_PORT: int = 8007
     LLAMA_PARSE_API_KEY: str = ""
     model_config = SettingsConfigDict(env_file=".env")
     
--- vectordbs/Milvus/main.py
+++ vectordbs/Milvus/main.py
@@ -1,6 +1,7 @@
 from pymilvus import MilvusClient
 import fastapi
 
+app = fastapi.FastAPI()
 
 client = MilvusClient("./database/milvus_demo.db")
 
@@ -26,9 +27,20 @@ def upload_pdf_to_vector_db(dataWithEmbeddings: dict | list[dict], collection_na
 def get_context_with_filters(collection_name, theme, subtheme, query: list):
     respuesta = client.search(
         data=[query], ##Valor que buscamos
-        anns_field="embedding", ##Campo con el que comparamos el embedding de consulta
-        param={"metric_type": "L2",
-            "params": {"nprobe":2}}, ##Definimos el tipo de metrica | nprobe 2 => 2 centroids INVESTIGAR ESTOS PARAMETROS POR AHORA LO DEJO DEFAULT
-        limit=5, ##Limita a 3 resultados por busqueda
-        output_fields=["texto"] ##Campo que queremos que devuelva
+        collection_name = collection_name,
+        #anns_field="embedding", ##Campo con el que comparamos el embedding de consulta
+        param={"metric_type": "COSINE", "params": {"nprobe":2}}, ##Definimos el tipo de metrica | nprobe 2 => 2 centroids INVESTIGAR ESTOS PARAMETROS POR AHORA LO DEJO DEFAULT
+        limit=5, ##Limita a 5 resultados por busqueda
+        #output_fields=["texto"] ##Campo que queremos que devuelva
     )
+    return respuesta
+
+
+
+@app.post("/upload-embeddings")
+def upload(dataWithEmbeddings: dict | list[dict], collection_name):
+    upload_pdf_to_vector_db(dataWithEmbeddings, collection_name)
+
+@app.post("/get-context")
+def get_context(collection_name, theme, subtheme, query: list):
+    return get_context_with_filters(collection_name, theme, subtheme, query)
