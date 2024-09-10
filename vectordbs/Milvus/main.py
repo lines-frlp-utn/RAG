83483648875a9a614ac8d913141dda44bb4ce496
Ivan_Vijandi <ivijandi75@gmail.com>
2024-09-09 22:17:42 -0300

    schema and index_params

--- vectordbs/Milvus/main.py
+++ vectordbs/Milvus/main.py
@@ -1,12 +1,13 @@
 import fastapi
 import json
 from pydantic import BaseModel
+from pymilvus import DataType
 
 app = fastapi.FastAPI()
 
 from pymilvus import MilvusClient
 client = MilvusClient(uri="http://milvus-standalone:19530")
-
+index_params = client.prepare_index_params()
 class EmbeddingData(BaseModel):
     dataWithEmbeddings: list[dict]
     collection_name: str
@@ -15,13 +16,41 @@ class QueryData(BaseModel):
     collection_name: str
     query: list
 
-def upload_pdf_to_vector_db(dataWithEmbeddings, collection_name):
+def create_schema():
+    schema = client.create_schema(
+        auto_id=False,
+        enable_dynamic_field=True,
+    )
+    
+    schema.add_field(field_name="id", datatype=DataType.INT64, is_primary=True)
+    schema.add_field(field_name="vector", datatype=DataType.FLOAT_VECTOR, dim=384)
+    schema.add_field(field_name="text", datatype=DataType.STRING)
+    
+    return schema
 
+
+def upload_pdf_to_vector_db(dataWithEmbeddings, collection_name):
     if client.has_collection(collection_name=collection_name):
         client.drop_collection(collection_name=collection_name)
+    
+    schema = create_schema()
+    
     client.create_collection(
         collection_name=collection_name,
+        schema=schema,
         dimension=384,  # The vectors we will use in this demo has 768 dimensions
+    )
+    index_params.add_index(
+        field_name="vector",
+        metric_type="COSINE",
+        index_type="IVF_FLAT",
+        index_name="vector_index", 
+        params={ "nlist": 128 }
+    )
+    client.create_index(
+        collection_name=collection_name,
+        index_params=index_params,
+        sync=False # Whether to wait for index creation to complete before returning. Defaults to True.
     )
     result = client.insert(
         collection_name = collection_name,
