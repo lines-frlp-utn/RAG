b3f0fa4160849c1e341edc5071ffb0d589678bd7
NachoL24 <ignaciolances24@gmail.com>
2024-09-16 13:31:13 -0300

    changed collection name on retrieving

--- chainlit_app/app/main.py
+++ chainlit_app/app/main.py
@@ -97,7 +97,7 @@ async def vectordb_results_step(query):
     settings = cl.user_session.get("settings")
     query_embedding = await cl.make_async(embedding_generator.get_embeddings)(query)
     results = await cl.make_async(get_context_from_db)(
-        collection_name=settings["collection"],
+        collection_name=collection_name,
         query=query_embedding,
     )
     cl.context.current_step.output = results
