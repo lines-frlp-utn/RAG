4f58651b6a998a00feb62142fcca87e890fc8949
MorettiFrancisco <fmoretti@alu.frlp.utn.edu.ar>
2024-09-20 03:05:45 +0000

    Refactor pdfExtractor.py to use fitz library for extracting text from PDFs

--- chainlit_app/app/pdfExtractor.py
+++ chainlit_app/app/pdfExtractor.py
@@ -1,10 +1,15 @@
-import pymupdf  # PyMuPDF
+import fitz
 
 def extract_text_from_pdf(pdf_path):
-    doc = pymupdf.open(pdf_path)
-    texts = []
-    for page_num in range(len(doc)):
-        page = doc.load_page(page_num)
-        text = page.get_text()
-        texts.append(text)
-    return texts
+    try:
+        doc = fitz.open(pdf_path)
+        texts = []
+        for page_num in range(len(doc)):
+            page = doc.load_page(page_num)
+            text = page.get_text("text")  # Especifica el formato si es necesario
+            texts.append(text)
+        return texts
+    except Exception as e:
+        print(f"Error al extraer texto del PDF: {e}")
+        return []
+
