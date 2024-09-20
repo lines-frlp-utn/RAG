8c5725a7e170c3bd407c8d012db85ed05bd9c414
tomasz <zubik.tomas@gmail.com>
2024-09-20 20:43:13 -0300

    Revert "Refactor pdfExtractor.py to use fitz library for extracting text from PDFs"
    This reverts commit 4f58651b6a998a00feb62142fcca87e890fc8949.

--- chainlit_app/app/pdfExtractor.py
+++ chainlit_app/app/pdfExtractor.py
@@ -1,15 +1,10 @@
-import fitz
+import pymupdf  # PyMuPDF
 
 def extract_text_from_pdf(pdf_path):
-    try:
-        doc = fitz.open(pdf_path)
-        texts = []
-        for page_num in range(len(doc)):
-            page = doc.load_page(page_num)
-            text = page.get_text("text")  # Especifica el formato si es necesario
-            texts.append(text)
-        return texts
-    except Exception as e:
-        print(f"Error al extraer texto del PDF: {e}")
-        return []
-
+    doc = pymupdf.open(pdf_path)
+    texts = []
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        text = page.get_text()
+        texts.append(text)
+    return texts
