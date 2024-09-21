48ef7ade2fb5af9cf883a7e2feb51d399a4061c0
tomaszbk-pc <zubik.tomas@gmail.com>
2024-09-21 12:13:46 -0300

    refactor: merge composes

--- chainlit_app/app/aim_tracker.py
+++ chainlit_app/app/aim_tracker.py
@@ -2,7 +2,7 @@ from langchain_community.callbacks import AimCallbackHandler
 from langchain_core.callbacks import StdOutCallbackHandler
 
 aim_callback = AimCallbackHandler(
-    repo="aim://server:53800",
+    repo="aim://aim-server:53800",
     experiment_name="Lines chat",
 )
 
