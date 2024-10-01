6e65d72022abcd84d76a0f9f5e6c5d5d83e99cd2
Francisco Lucich <franciscoivlucich@gmail.com>
2024-10-01 16:23:49 -0300

    added basic aim run managment functions

--- chainlit_app/app/aim_tracker.py
+++ chainlit_app/app/aim_tracker.py
@@ -1,5 +1,6 @@
 from langchain_community.callbacks import AimCallbackHandler
 from langchain_core.callbacks import StdOutCallbackHandler
+from aim import Run, Text
 
 aim_callback = AimCallbackHandler(
     repo="aim://aim-server:53800",
@@ -7,3 +8,19 @@ aim_callback = AimCallbackHandler(
 )
 
 callbacks = [StdOutCallbackHandler(), aim_callback]
+
+def start_aim_run(): 
+    return Run(repo="aim://aim-server:53800",
+    experiment_name="Lines chat"),
+
+
+def end_aim_run(aim_run):
+    aim_run.close()
+
+def track_param(aim_run,name,value):
+    aim_run[name]= value
+
+
+def track_text(aim_run,name,text):
+    aim_text = Text(text)
+    aim_run.track(aim_text,name=name)
