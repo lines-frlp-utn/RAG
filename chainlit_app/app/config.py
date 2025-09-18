796ae123241d6acacf7d8accfa5620cbfa6b6e37
Franquinios <francopietrantuono999@gmail.com>
2025-09-17 23:36:46 -0300

    feat(config.py): add environment-based configuration loading and implement Pydantic Field validation for config settings

--- chainlit_app/app/config.py
+++ chainlit_app/app/config.py
@@ -1,17 +1,23 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import Field
+import os
 
 class Settings(BaseSettings):
-    MODEL_URL: str
-    DB_URL: str
-    MODEL_PORT: int
-    DB_PORT: int
-    LLAMA_PARSE_API_KEY: str | None
-    USE_AIM: bool
-    CHAINLIT_AUTH_SECRET: str | None
-    OAUTH_GOOGLE_CLIENT_ID: str | None
-    OAUTH_GOOGLE_CLIENT_SECRET: str | None
-    DATABASE_URL: str
+    MODEL_URL: str = Field(..., env="MODEL_URL")
+    DB_URL: str = Field(..., env="DB_URL")
+    MODEL_PORT: int = Field(..., env="MODEL_PORT")
+    DB_PORT: int = Field(..., env="DB_PORT")
+    LLAMA_PARSE_API_KEY: str | None = Field(None, env="LLAMA_PARSE_API_KEY")
+    USE_AIM: bool = Field(..., env="USE_AIM")
+    LITERAL_API_KEY: str | None = Field(None, env="LITERAL_API_KEY")
+    USERS_API_URL: str | None = Field(None, env="USERS_API_URL")
+    USERS_API_PORT: int | None = Field(None, env="USERS_API_PORT")
+    CHAINLIT_AUTH_SECRET: str | None = Field(None, env="CHAINLIT_AUTH_SECRET")
+    OAUTH_GOOGLE_CLIENT_ID: str | None = Field(None, env="OAUTH_GOOGLE_CLIENT_ID")
+    OAUTH_GOOGLE_CLIENT_SECRET: str | None = Field(None, env="OAUTH_GOOGLE_CLIENT_SECRET")
 
+    class Config:
+        env_file = ".env.test" if os.getenv("TESTING") else ".env"
+        extra = "ignore"
 
 conf = Settings()
