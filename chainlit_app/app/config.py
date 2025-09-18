853438511434a728ae427639fd8fdde5375514b5
Franquinios <francopietrantuono999@gmail.com>
2025-09-18 19:18:07 -0300

    chore: add DEFAULT_MAX_LENGTH env variable

--- chainlit_app/app/config.py
+++ chainlit_app/app/config.py
@@ -15,6 +15,7 @@ class Settings(BaseSettings):
     CHAINLIT_AUTH_SECRET: str | None = Field(None, env="CHAINLIT_AUTH_SECRET")
     OAUTH_GOOGLE_CLIENT_ID: str | None = Field(None, env="OAUTH_GOOGLE_CLIENT_ID")
     OAUTH_GOOGLE_CLIENT_SECRET: str | None = Field(None, env="OAUTH_GOOGLE_CLIENT_SECRET")
+    DEFAULT_MAX_LENGTH: int | None = Field(None, env="DEFAULT_MAX_LENGTH")
 
     class Config:
         env_file = ".env.test" if os.getenv("TESTING") else ".env"
